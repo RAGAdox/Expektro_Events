99aab56191f85abd86d1ac77e966c232089e7398
RAGAdox <rishitech882@gmail.com>
2019-03-04 21:29:58 +0530

    Mobile Issue Solved

--- js/newBs.js
+++ js/newBs.js
@@ -4,24 +4,32 @@ var curEve;
 console.log(document.activeElement);
 //var article=document.getElementsByTagName("article");
 //document.getElementById("sec").className="bespoke-active bespoke-slide"
-var mySwipeIt = new SwipeIt('.box');
+var mySwipeIt = new SwipeIt(".box");
 var list = document.getElementsByTagName("section");
 list[3].className = "bespoke-active bespoke-slide";
 var cur = 3;
 for (var i = cur + 1; i < list.length; i++) {
-  list[i].className = "bespoke-inactive bespoke-after bespoke-after-" + (i - cur) + " bespoke-slide";
+  list[i].className =
+    "bespoke-inactive bespoke-after bespoke-after-" +
+    (i - cur) +
+    " bespoke-slide";
 }
 for (var i = 0; i < cur; i++) {
-  list[i].className = "bespoke-slide bespoke-inactive bespoke-before bespoke-before-" + (cur - i);
+  list[i].className =
+    "bespoke-slide bespoke-inactive bespoke-before bespoke-before-" + (cur - i);
 }
 function leftSec() {
   if (cur < list.length - 1 && inSection == true) {
     cur = cur + 1;
     for (var i = 0; i < cur; i++) {
-      list[i].className = "bespoke-slide bespoke-inactive bespoke-before bespoke-before-" + (cur - i);
+      list[i].className =
+        "bespoke-slide bespoke-inactive bespoke-before bespoke-before-" +
+        (cur - i);
     }
     for (var i = cur; i < list.length; i++) {
-      list[i].className = "bespoke-slide bespoke-inactive bespoke-before bespoke-after-" + (i - cur);
+      list[i].className =
+        "bespoke-slide bespoke-inactive bespoke-before bespoke-after-" +
+        (i - cur);
     }
     list[cur].className = "bespoke-active bespoke-slide";
   }
@@ -30,23 +38,27 @@ function rightSec() {
   if (cur > 0 && inSection == true) {
     cur = cur - 1;
     for (var i = 0; i < cur; i++) {
-      list[i].className = "bespoke-slide bespoke-inactive bespoke-before bespoke-before-" + (cur - i);
+      list[i].className =
+        "bespoke-slide bespoke-inactive bespoke-before bespoke-before-" +
+        (cur - i);
     }
     for (var i = cur; i < list.length; i++) {
-      list[i].className = "bespoke-slide bespoke-inactive bespoke-before bespoke-after-" + (i - cur);
+      list[i].className =
+        "bespoke-slide bespoke-inactive bespoke-before bespoke-after-" +
+        (i - cur);
     }
     list[cur].className = "bespoke-active bespoke-slide";
   }
 }
 
 mySwipeIt
-  .on('swipeLeft', function (e) {
+  .on("swipeLeft", function(e) {
     leftSec();
   })
-  .on('swipeRight', function (e) {
+  .on("swipeRight", function(e) {
     rightSec();
   })
-  .on('swipeUp', function (e) {
+  .on("swipeUp", function(e) {
     for (var i = 0; i < list.length; i++) {
       if (i == cur) {
         console.log("swipe up event");
@@ -55,16 +67,14 @@ mySwipeIt
         list[i].style.top = "0%";
         list[i].style.left = "0%";
         list[i].style.borderRadius = "0px";
-
-      }
-      else {
+      } else {
         list[i].style.display = "none";
       }
     }
 
     inSection = false;
   });
-document.addEventListener('keydown', function (e) {
+document.addEventListener("keydown", function(e) {
   var key = e.which;
   if (key == 39 && cur < list.length && inSection == true) {
     leftSec();
@@ -81,7 +91,6 @@ document.addEventListener('keydown', function (e) {
         list[i].style.top = "10%";
         list[i].style.left = "20%";
         list[i].style.borderRadius = "30px";
-
       }
       list[i].style.display = "block";
     }
@@ -95,9 +104,7 @@ document.addEventListener('keydown', function (e) {
         list[i].style.top = "0%";
         list[i].style.left = "0%";
         list[i].style.borderRadius = "0px";
-
-      }
-      else {
+      } else {
         list[i].style.display = "none";
       }
     }
@@ -120,57 +127,70 @@ function showEvents(str) {
   if(document.getElementById(str+"_Eve")!=null)
     document.getElementById(str+"_Eve").style.opacity="1";
   /*change the parameters to desired*/
-
 }
 function back(event) {
-
-if(inSection==false){
-  console.log("inSection is false");
-  event=event||window.event;
+  if (inSection == false) {
+    console.log("inSection is false");
+    event = event || window.event;
     for (var i = 0; i < list.length; i++) {
       if (i == cur) {
         inSection = true;
-        if(window.innerWidth<=570)
-          {
-            list[i].style.width="80%";
-            list[i].style.left="10%";
-          }
-        else
-          {
-            list[i].style.width = "60%";
-            list[i].style.left = "20%";
-          }
+        if (window.innerWidth <= 570) {
+          list[i].style.width = "80%";
+          list[i].style.left = "10%";
+        } else {
+          list[i].style.width = "60%";
+          list[i].style.left = "20%";
+        }
         list[i].style.height = "80%";
         list[i].style.top = "10%";
-        
-        list[i].style.borderRadius = "30px";
 
+        list[i].style.borderRadius = "30px";
       }
       list[i].style.display = "block";
     }
-    inSection=true;
+    inSection = true;
   }
 
   event.stopPropagation();
 }
 function showSec() {
-if(inSection==true && cur!=3){
-  console.log("inSection is true");
-  
-  for (var i = 0; i < list.length; i++) {
-    if (i == cur) {
-      list[i].style.width = "100%";
-      list[i].style.height = "100%";
-      list[i].style.top = "0%";
-      list[i].style.left = "0%";
-      list[i].style.borderRadius = "0px";
+  if (
+    inSection == true &&
+    cur != 3 &&
+    window.innerHeight <= window.innerWidth
+  ) {
+    console.log("inSection is true");
 
+    for (var i = 0; i < list.length; i++) {
+      if (i == cur) {
+        list[i].style.width = "100%";
+        list[i].style.height = "100%";
+        list[i].style.top = "0%";
+        list[i].style.left = "0%";
+        list[i].style.borderRadius = "0px";
+      } else {
+        list[i].style.display = "none";
+      }
     }
-    else {
-      list[i].style.display = "none";
-    }
+    inSection = false;
   }
-  inSection = false;
 }
+function showSecm() {
+  if (inSection == true && cur != 3) {
+    console.log("inSection is true");
 
+    for (var i = 0; i < list.length; i++) {
+      if (i == cur) {
+        list[i].style.width = "100%";
+        list[i].style.height = "100%";
+        list[i].style.top = "0%";
+        list[i].style.left = "0%";
+        list[i].style.borderRadius = "0px";
+      } else {
+        list[i].style.display = "none";
+      }
+    }
+    inSection = false;
+  }
 }
